f97b69ec1c238d554111f4407e9a527ecfccd088
LittleGene8 <karnati.mayank@gmail.com>
2020-08-01 14:50:57 -0400

    Added player to the screen at the middle.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
+from pathlib import Path
 
 pygame.init()
 
 # Create a window
 
 window = pygame.display.set_mode((800, 600))
+
+# Player
+
+player_img = pygame.image.load('Assets/player.png')
+playerX = 380
+playerY = 280
 
 # Colors
 
@@ -13,14 +20,22 @@ black = (0,0,0)
 # Game Variables
 run = True
 
+# Draw Function
+
+def draw():
+
+  window.blit(player_img, (playerX, playerY))
+
 # Game Loop
 
 while run:
 
   window.fill(black)
 
+  draw()
+
+  pygame.display.update()
+
   for event in pygame.event.get():
     if event == pygame.QUIT:
       run = False
-
-  pygame.display.update()

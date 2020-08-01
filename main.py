e304b48934c46181a62dd1ce2020057ac8e4fc87
LittleGene8 <karnati.mayank@gmail.com>
2020-08-01 15:38:27 -0400

    Player movement complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from pathlib import Path
+import math
 
 pygame.init()
 
@@ -10,32 +10,73 @@ window = pygame.display.set_mode((800, 600))
 # Player
 
 player_img = pygame.image.load('Assets/player.png')
-playerX = 380
+playerX = 400
 playerY = 280
+playerX_change = 0
+playerY_change = 0
+player_angle = 90
+play_ang_change = 0
 
 # Colors
 
-black = (0,0,0)
+black = (0, 0, 0)
 
 # Game Variables
 run = True
 
-# Draw Function
+
+# Game Functions
+
+
+def get_speeds(ang, vel):
+    ang_rad = math.radians(ang)
+    vel_x = math.cos(ang_rad) * vel
+    vel_y = math.sin(ang_rad) * vel
+    return (vel_x, vel_y)
+
 
 def draw():
+    global player_angle
+    global playerX
+    global playerY
 
-  window.blit(player_img, (playerX, playerY))
+    # Player
+
+    player_img_copy = pygame.transform.rotozoom(player_img, player_angle, 1)  # rotates player
+
+    # Determines amount to subtract by to center
+    center_x = player_img_copy.get_width() / 2
+    center_y = player_img_copy.get_height() / 2
+
+    window.blit(player_img_copy, (playerX - center_x, playerY - center_y))  # centers and draws image
+
+    player_angle += play_ang_change  # Adds onto angle based on controls
+    playerX += playerX_change
+    playerY -= playerY_change
+
 
 # Game Loop
 
 while run:
 
-  window.fill(black)
+    window.fill(black)
 
-  draw()
+    draw()
 
-  pygame.display.update()
+    pygame.display.update()
 
-  for event in pygame.event.get():
-    if event == pygame.QUIT:
-      run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                play_ang_change = 7.5
+            if event.key == pygame.K_RIGHT:
+                play_ang_change = -7.5
+            if event.key == pygame.K_UP:
+                playerX_change, playerY_change = get_speeds(player_angle, 3)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                play_ang_change = 0
+            if event.key == pygame.K_UP:
+                playerX_change, playerY_change = 0, 0

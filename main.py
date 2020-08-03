05c2b3ba5e5f5163f92d6eb05e1a09c9a2c8dbf8
LittleGene8 <68621915+LittleGene8@users.noreply.github.com>
2020-08-03 14:17:37 -0400

    Player movement update
    The movement of the player is now complete. In addition to the regular movements of the spaceship, boundaries to the player have also been added. The player also has the no friction effect where the spaceship drags a little bit even after the button is released.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import pygame
 import math
+import os.path
 
 pygame.init()
 
@@ -11,13 +12,14 @@ window = pygame.display.set_mode((800, 600))
 
 # Player
 
-player_img = pygame.image.load('Assets/player.png')
+player_img = pygame.image.load(os.path.join('Assets','player.png'))
 playerX = 400
 playerY = 280
 playerX_change = 0
 playerY_change = 0
 player_angle = 90
 play_ang_change = 0
+speed = 1
 
 # Colors
 
@@ -25,60 +27,85 @@ black = (0, 0, 0)
 
 # Game Variables
 run = True
-
+drag = False
 
 # Game Functions
 
 
 def get_speeds(ang, vel):
-    ang_rad = math.radians(ang)
-    vel_x = math.cos(ang_rad) * vel
-    vel_y = math.sin(ang_rad) * vel
-    return (vel_x, vel_y)
+  ang_rad = math.radians(ang)
+  vel_x = math.cos(ang_rad) * vel
+  vel_y = math.sin(ang_rad) * vel
+  return (vel_x, vel_y)
 
 
 def draw():
-    global player_angle
-    global playerX
-    global playerY
+  global player_angle
+  global playerX
+  global playerY
+  global speed
+  global playerX_change
+  global playerY_change
 
-    # Player
+  # Player
 
-    player_img_copy = pygame.transform.rotozoom(player_img, player_angle, 1)  # rotates player
+  player_img_copy = pygame.transform.rotozoom(player_img, player_angle, 1)  # rotates player
 
-    # Determines amount to subtract by to center
-    center_x = player_img_copy.get_width() / 2
-    center_y = player_img_copy.get_height() / 2
+  # Determines amount to subtract by to center
+  center_x = player_img_copy.get_width() / 2
+  center_y = player_img_copy.get_height() / 2
 
-    window.blit(player_img_copy, (playerX - center_x, playerY - center_y))  # centers and draws image
+  window.blit(player_img_copy, (playerX - center_x, playerY - center_y))  # centers and draws image
 
-    player_angle += play_ang_change  # Adds onto angle based on controls
-    playerX += playerX_change
-    playerY -= playerY_change
+  # Drag
+
+  if drag == True:
+   speed *= 0.991
+   playerX_change, playerY_change = get_speeds(temp_angle, speed)
+
+  player_angle += play_ang_change  # Adds onto angle based on controls
+  playerX += playerX_change
+  playerY -= playerY_change
+
+  # Adding Boundaries
+
+  if playerX <= 16:
+    playerX = 16
+  if playerX >= 780:
+    playerX = 780
+  if playerY <= 16:
+    playerY = 16
+  if playerY >= 580:
+    playerY = 580
+
+
 
 
 # Game Loop
 
 while run:
 
-    window.fill(black)
+  window.fill(black)
 
-    draw()
+  draw()
 
-    pygame.display.update()
+  pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                play_ang_change = 7.5
-            if event.key == pygame.K_RIGHT:
-                play_ang_change = -7.5
-            if event.key == pygame.K_UP:
-                playerX_change, playerY_change = get_speeds(player_angle, 3)
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                play_ang_change = 0
-            if event.key == pygame.K_UP:
-                playerX_change, playerY_change = 0, 0
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        play_ang_change = 0.75
+      if event.key == pygame.K_RIGHT:
+        play_ang_change = -0.75
+      if event.key == pygame.K_UP:
+        drag = False
+        speed = 1
+        playerX_change, playerY_change = get_speeds(player_angle, speed)
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        play_ang_change = 0
+      if event.key == pygame.K_UP:
+        temp_angle = player_angle
+        drag = True
